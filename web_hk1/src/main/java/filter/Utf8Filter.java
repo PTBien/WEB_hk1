a4800320e604409917973cf03d32125b14074fec
PTBien <20130203@st.hcmuaf.edu.vngit config user.email 20130203@st.hcmuaf.edu.vn>
2025-01-10 18:44:41 +0700

    test commit

--- web_hk1/src/main/java/filter/Utf8Filter.java
+++ web_hk1/src/main/java/filter/Utf8Filter.java
@@ -9,9 +9,13 @@ public class Utf8Filter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filter)
+
+
+
+
             throws IOException, ServletException {
         req.setCharacterEncoding("utf8");
         resp.setCharacterEncoding("utf8");
-        filter.doFilter(req, resp);
+        filter.doFilter(req, resp) ;
     }
 }
